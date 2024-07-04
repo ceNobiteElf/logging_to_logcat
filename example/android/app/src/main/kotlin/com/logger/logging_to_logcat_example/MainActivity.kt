d244e59caf8c7b50f3f31d31578bf51302021dce
Con Momberg <conm@discovery.co.za>
2024-07-04 22:28:51 +0200

    Cleanup and updated example for AGP 8

--- example/android/app/src/main/kotlin/com/logger/logging_to_logcat_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/logger/logging_to_logcat_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.logger.logging_to_logcat_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
